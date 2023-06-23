99e12d04bfbd81833a3d4fdc906e885bc59b7d18
Tintanaa <miiagkov123456@gmail.com>
2023-06-23 19:44:40 +0600

    attempt

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,25 +4,29 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        double x1;
+        double x2;
+        double[] array = new double[2];
+        double eps = 1e-9;
+        double d = b * b - 4 * a * c;
+        if (-eps < a && a < eps)
+        {
+            throw new System.ArgumentException();
+        }
         if (a == 0 || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
         {
             throw new ArgumentException("Ошибка");
         }
-        double x1;
-        double x2;
-        double[] array = new double[2];
-        double d = b * b - 4 * a * c;
-        //double d = (b - 2 * Math.Sqrt(a * c)) * (b + 2 * Math.Sqrt(a * c));
-        if (d<0 && !(-Single.Epsilon < d && d < 0))
+        if (d<=eps)
         {
             array = new double[0];
         }
-        if (d == 0 || d < Single.Epsilon)
+        else if (-eps < d && d < eps)
         {
-            x1 = 2*c/-b;
+            x1 = -b/(2*a);
             array = new double[] { x1 };
         }
-        if(d>0|| d >= Single.Epsilon)
+        else 
         {
             x1 = (2*c)/-(b+Math.Sign(b)*Math.Sqrt(d));
             x2 = c / (a*x1);
