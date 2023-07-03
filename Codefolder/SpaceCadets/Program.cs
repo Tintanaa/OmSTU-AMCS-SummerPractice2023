5c152e8bb87af0d858e0f3030baca48981aa04f9
Tintanaa <miiagkov123456@gmail.com>
2023-07-03 21:39:24 +0600

    changes program.cs

--- Codefolder/SpaceCadets/Program.cs
+++ Codefolder/SpaceCadets/Program.cs
@@ -16,62 +16,42 @@ namespace SpaceCadets
     {
         static int Main(string[] args)
         {
-            var fileToReadOption = new Option<string>(
-            "--filetoread",
-            description: "Path to the file to read."
-            );
-            var fileToWriteOption = new Option<string>(
-                "--filetowrite",
-                description: "Path to the file to write."
-            );
-            var rootCommand = new RootCommand
+            JSONParser parser = new JSONParser();
+            JSONMaker maker = new JSONMaker();
+            JSONWriter writer = new JSONWriter();
+            Actions actions = new Actions();
+            string filepathread = args[0];
+            string filepathwrite = args[1];
+            var taskdata = parser.JSONParseFilter(filepathread);
+            Console.WriteLine(taskdata.Data);
+            switch (taskdata.TaskName)
             {
-            fileToReadOption,
-            fileToWriteOption
-            };
-            rootCommand.Description = "filetoreadOption for path where file exists. fileToWriteOption for path where file saves";
-            rootCommand.Handler = CommandHandler.Create<string,string>(
-                (fileToRead, fileToWrite) =>
-            {
-                // Use the fileToRead and fileToWrite variables as needed in your application
-                JSONParser parser = new JSONParser();
-                JSONMaker maker = new JSONMaker();
-                JSONWriter writer = new JSONWriter();
-                Actions actions = new Actions();
-                string filepathread = args[0];
-                string filepathwrite = args[1];
-                var taskdata = parser.JSONParseFilter(filepathread);
-                Console.WriteLine(taskdata.Data);
-                switch (taskdata.TaskName)
-                {
-                    case "GetStudentsWithHighestGPA":
-                        {
-                            var result = actions.GetStudentsWithHighestGPA(taskdata.Data);
-                            Console.WriteLine(result[0]);
-                            JObject js = new JObject();
-                            js = maker.JSONmaker(result);
-                            writer.JSONwriter(js, filepathwrite);
-                            break;
-                        }
-                    case "CalculateGPAByDiscipline":
-                        {
-                            var result = actions.CalculateGPAByDiscipline(taskdata.Data);
-                            JObject js = new JObject();
-                            js = maker.JSONmaker(result);
-                            writer.JSONwriter(js, filepathwrite);
-                            break;
-                        }
-                    case "GetBestGroupsByDiscipline":
-                        {
-                            var result = actions.GetBestGroupsByDiscipline(taskdata.Data);
-                            JObject js = new JObject();
-                            js = maker.JSONmaker(result);
-                            writer.JSONwriter(js, filepathwrite);
-                            break;
-                        }
-                }
-            });
-            return rootCommand.Invoke(args);
+                case "GetStudentsWithHighestGPA":
+                    {
+                        var result = actions.GetStudentsWithHighestGPA(taskdata.Data);
+                        Console.WriteLine(result[0]);
+                        JObject js = new JObject();
+                        js = maker.JSONmaker(result);
+                        writer.JSONwriter(js, filepathwrite);
+                        break;
+                    }
+                case "CalculateGPAByDiscipline":
+                    {
+                        var result = actions.CalculateGPAByDiscipline(taskdata.Data);
+                        JObject js = new JObject();
+                        js = maker.JSONmaker(result);
+                        writer.JSONwriter(js, filepathwrite);
+                        break;
+                    }
+                case "GetBestGroupsByDiscipline":
+                    {
+                        var result = actions.GetBestGroupsByDiscipline(taskdata.Data);
+                        JObject js = new JObject();
+                        js = maker.JSONmaker(result);
+                        writer.JSONwriter(js, filepathwrite);
+                        break;
+                    }
+            }
         }
     }
 }
